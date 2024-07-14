14addbc206788f80027427aef1d7fd1cf0d43e4f
Sergey Otrubyannikov <s.otrubyannikov@gmail.com>
2024-07-14 15:15:39 +0300

    fix5

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,26 @@ function Footer() {
   return (
     <footer className="footer">
       <ul className="footer__list">
-        <li className="footer__item">Помощь</li>
-        <li className="footer__item">Обратная связь</li>
-        <li className="footer__item">Разработчикам</li>
-        <li className="footer__item">Условия использования</li>
+        <li className="footer__item">
+          <a className="footer__link" href="#">
+            Помощь
+          </a>
+        </li>
+        <li className="footer__item">
+          <a className="footer__link" href="#">
+            Обратная связь
+          </a>
+        </li>
+        <li className="footer__item">
+          <a className="footer__link" href="#">
+            Разработчикам
+          </a>
+        </li>
+        <li className="footer__item">
+          <a className="footer__link" href="#">
+            Условия использования
+          </a>
+        </li>
       </ul>
       <div className="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
     </footer>
